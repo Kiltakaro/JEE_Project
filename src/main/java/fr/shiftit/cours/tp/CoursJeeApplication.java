b517ae40ab55e09b5e5a37d58edb7b4593b5acd4
Aventin <farretaven@cy-tech.fr>
2023-10-20 08:16:58 +0200

    add dev to aventin

---
+++ src/main/java/fr/shiftit/cours/tp/CoursJeeApplication.java
@@ -0,0 +1,13 @@
+package fr.shiftit.cours.tp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoursJeeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoursJeeApplication.class, args);
+    }
+
+}
