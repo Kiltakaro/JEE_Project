2a5692fa78fa0107a5629d48331988a867cfb269
ChomeMecho <cornec.dorian@orange.fr>
2023-10-23 01:35:51 +0200

    add of getters and setters for the User class (forgotten before)

--- src/main/java/database/User.java
+++ src/main/java/database/User.java
@@ -23,5 +23,43 @@ public class User {
     // Type of user
     private String type;
 
+    public Long getId() {
+        return id;
+    }
 
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEncryptedPassword() {
+        return encryptedPassword;
+    }
+
+    public void setEncryptedPassword(String encryptedPassword) {
+        this.encryptedPassword = encryptedPassword;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
 }
