0402a08d64e8907178327a76a78bdd1fac1d8763
ChomeMecho <cornec.dorian@orange.fr>
2023-10-25 10:53:27 +0200

    set up AnimeController
    Signed-off-by: ChomeMecho <cornec.dorian@orange.fr>

--- src/main/java/database/AnimeController.java
+++ src/main/java/database/AnimeController.java
@@ -1,4 +1,30 @@
 package database;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
 public class AnimeController {
+
+    @Autowired
+    AnimeRepository animeRepository;
+
+    @GetMapping("/addAnime")
+    public String showAnimeForm(Model model) {
+
+        return "addAnime";
+    }
+
+    @PostMapping("/addAnime")
+    public String addAnime(@ModelAttribute Anime anime) {
+
+        System.out.println("dir:"+anime);
+        animeRepository.save(anime);
+
+        return "redirect:/addAnime";
+    }
 }
