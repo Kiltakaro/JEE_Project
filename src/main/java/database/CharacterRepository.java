2a87a68c18bc5ce3254653ae5d4c0d829cec3392
ChomeMecho <cornec.dorian@orange.fr>
2023-10-25 10:43:53 +0200

    add of repository classes and AnimeController
    Signed-off-by: ChomeMecho <cornec.dorian@orange.fr>

---
+++ src/main/java/database/CharacterRepository.java
@@ -0,0 +1,12 @@
+package database;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CharacterRepository extends JpaRepository<Character, Long>{
+
+    List<Character> findByName(String lastName);
+}
