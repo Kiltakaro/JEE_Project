fa5a38c6af53c109e98c93b19f5bf78ca44d5612
Aventin <farretaven@cy-tech.fr>
2023-10-25 11:57:51 +0200

    accueil display w/ bdd

--- src/main/java/fr/shiftit/cours/tp/AccueilController.java
+++ src/main/java/fr/shiftit/cours/tp/AccueilController.java
@@ -7,10 +7,18 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import java.util.List;
+
 @Controller
 public class AccueilController {
+    @Autowired
+    private FilmRepository filmRepository;
     @GetMapping("/Accueil")
     public String Accueil(Model model) {
+        List<Film> films = filmRepository.findAll();
+
+        model.addAttribute("films", films);
+
         return "Accueil";
     }
 }
