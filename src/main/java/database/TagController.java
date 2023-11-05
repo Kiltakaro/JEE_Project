cff746bca45b9bb63805334a31c3e03f30cf1f70
Aventin <farretaven@cy-tech.fr>
2023-11-05 19:59:06 +0100

    add addTag

--- src/main/java/database/TagController.java
+++ src/main/java/database/TagController.java
@@ -6,6 +6,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
 import java.util.Comparator;
@@ -15,6 +16,11 @@ import java.util.List;
 public class TagController {
     @Autowired
     private AnimeRepository animeRepository;
+    private TagRepository tagRepository;
+    public TagController(AnimeRepository animeRepository, TagRepository tagRepository) {
+        this.animeRepository = animeRepository;
+        this.tagRepository = tagRepository;
+    }
     @GetMapping("/categories")
     public String getRecentAnime(Model model) {
         List<Anime> films = animeRepository.findAll();
@@ -22,6 +28,17 @@ public class TagController {
         List<Anime> recentAnimes = films.subList(0,films.size());
         model.addAttribute("recentAnimes", recentAnimes);
         return "categories";
+    }
+    @GetMapping("/addTag")
+    public String addCategorie(Model model) {
+        return "addTag";
+    }
+    @PostMapping("/addTag")
+    public String addCategorie(@RequestParam String name, Model model) {
+        Tag tag = new Tag();
+        tag.setName(name);
+        tagRepository.save(tag);
+        return "redirect:/categories";
     }
 
 
