d3957afd9fdfb8c7070267c5374c2e44d9129b21
Aventin <farretaven@cy-tech.fr>
2023-11-05 13:38:48 +0100

    add user to review

--- src/main/java/database/AnimeController.java
+++ src/main/java/database/AnimeController.java
@@ -4,11 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
 
@@ -16,7 +13,8 @@ import java.util.List;
 public class AnimeController {
 
     final AnimeService animeService;
-    
+    @Autowired
+    private AnimeRepository animeRepository;
 
     @Autowired
     public AnimeController(AnimeService animeService) {
@@ -63,8 +61,6 @@ public class AnimeController {
             return "redirect:/error"; // You can create an error page
         }
     }
-
-
 
 
 }
